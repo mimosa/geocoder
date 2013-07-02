ecaf39e1d67a151a46fc75aec449e8352ed0e985
Howl王 <mimosa@shareday.com>
2013-07-02 21:59:15 +0800

    身份证geo

--- app/app.rb
+++ app/app.rb
@@ -16,22 +16,33 @@ class API < Grape::API
     requires :loc, type: String, desc: "地址或IP"
   end
   get '/' do
-    location = params[:loc].force_encoding('utf-8')
     result   = {}
-    # 手机归属地查询
-    if (location.size == 11 && location.to_i > 13000000001)
-      geo = redis.hgetall(location[0..6])
+    location = params[:loc].force_encoding('utf-8')
+    
+    mobile = location[0..6] # 手机号段
+    if (location.size == 11 && mobile.to_i > 1300000)
+      geo = redis.hgetall(mobile)
       unless geo.empty?
         result[:provide] = geo['provide']
         result[:mobile]  = location
         location = geo['location']
       end
     end
+    
+    idcard = location[0..5] # 身份证号段
+    if (location.size == 18 && idcard.to_i > 110000)
+      geo = redis.hgetall(idcard)
+      unless geo.empty?
+        result[:idcard]  = location
+        location = geo['location']
+      end
+    end
+
     # IP 或 地址 查询
     unless location.nil?
       @geocoder = Geocoder.new
       geo  = @geocoder.get(location)
-      result.merge!(geo)
+      result.merge!(geo) unless geo.nil?
     end
     # 结果
     result
--- bin/migrate.sh
+++ bin/migrate.sh
@@ -4,7 +4,15 @@
 cat ./db/data.csv | awk -F "|" "BEGIN {x=1} {print \"HMSET\", \$1, \"provide\", \$2, \"location\", \$3 ; x++ }" | redis-cli > /dev/null 
 
 
-echo 'import data finished.'
+echo 'import mobile data finished.'
 echo 'try it now: '
 echo '  $ redis-cli HGET 1300002 location'
 echo '  "安徽巢湖"'
+
+cat ./db/id.csv | awk -F "|" "BEGIN {y=1} {print \"HMSET\", \$1, \"location\", \$2 ; y++ }" | redis-cli > /dev/null 
+
+
+echo 'import idcard data finished.'
+echo 'try it now: '
+echo '  $ redis-cli HGET 610103 location'
+echo '  "陕西省西安市碑林区"'
